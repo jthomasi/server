172ee92bacb2679de877b32e47cad6d2f2d1f0bc
Jesse Thomas <jayayetea@gmail.com>
2017-04-11 19:51:22 -0500

    using multiple server ports at once

--- server.js
+++ server.js
@@ -12,42 +12,30 @@
 // });
 
 var http = require('http');
+var PORT = 7000;
+var PORT_2 = 7500;
 var command = process.argv[2];
 
-if (command == "1"){
-
-	var PORT = 7000;
-
-	function handleRequest(request, response){
-		response.end("You look like flowers today! server path: "+request.url);
-	}
-
-	var server = http.createServer(handleRequest);
-
-	server.listen(PORT, function(){
-		console.log("Server is listening on: http://localhost:%s", PORT);
-	});
-
+function handleRequestNice(request, response){
+	response.end("You look like flowers today! server path: "+request.url);
 }
 
-else if (command == "2"){
+var server = http.createServer(handleRequestNice);
 
-	var PORT = 7500;
+server.listen(PORT, function(){
+	console.log("Server is listening on: http://localhost:%s", PORT);
+});
 
-	function handleRequest(request, response){
-		response.end("You look like poop today! server path: "+request.url);
-	}
-
-	var server = http.createServer(handleRequest);
-
-	server.listen(PORT, function(){
-		console.log("Server is listening on: http://localhost:%s", PORT);
-	});
-
+function handleRequestMean(request, response){
+	response.end("You look like poop today! server path: "+request.url);
 }
 
-else {
-	console.log("Enter a proper command");
-}
+var server = http.createServer(handleRequestMean);
+
+server.listen(PORT_2, function(){
+	console.log("Server is listening on: http://localhost:%s", PORT_2);
+});
+
+
 
 
